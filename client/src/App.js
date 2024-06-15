8fa64feb4e3b0b32fdc39993ab233929545c3f50
Libin-1998 <libinninteen98@gmail.com>
2024-06-15 09:18:12 +0530

    location permission

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/Home';
+import Location from './components/location/Location';
 
 function App() {
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      <Location/>
     </>
   );
 }
---
+++ client/src/components/location/Location.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function Location() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [status, setStatus] = useState(null);
+
+  const requestLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          setStatus("Location obtained successfully!");
+        },
+        // (error) => {
+        //   switch (error.code) {
+        //     case error.PERMISSION_DENIED:
+        //       setStatus("User denied the request for Geolocation.");
+        //       break;
+        //     case error.POSITION_UNAVAILABLE:
+        //       setStatus("Location information is unavailable.");
+        //       break;
+        //     case error.TIMEOUT:
+        //       setStatus("The request to get user location timed out.");
+        //       break;
+        //     case error.UNKNOWN_ERROR:
+        //       setStatus("An unknown error occurred.");
+        //       break;
+        //     default:
+        //       setStatus("An error occurred while fetching location.");
+        //   }
+        // }
+      );
+    } else {
+      setStatus("Geolocation is not supported by your browser.");
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Location Permission Request</h1>
+        <button onClick={requestLocation}>Get Location</button>
+        <p>Status: {status}</p>
+        {location.latitude && location.longitude && (
+          <p>
+            Latitude: {location.latitude}, 
+            Longitude: {location.longitude},
+          </p>
+         )}
+      </div>
+    </>
+  );
+}
